94a0ca2f5ccf92b4357fe1d4f68f082532f5ca3c
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2021-11-27 00:09:11 +0200

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Demo
+          Learn React Demo 2
         </a>
       </header>
     </div>
